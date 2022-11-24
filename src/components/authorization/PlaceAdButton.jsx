991c0972abe4d8dcda2ad68059264dae83e00a76
Luciana Margoto <margotoluciana@gmail>
2022-11-24 15:31:11 +0100

    added useeffect and user

--- src/components/authorization/PlaceAdButton.jsx
+++ src/components/authorization/PlaceAdButton.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import * as UsersApi from '../../api/UsersApi';
 import { useNavigate } from "react-router-dom";
+
 
 const BASE_URL = process.env.REACT_APP_BACKEND_BASE_SERVER_URL;
 
 const PlaceAdButton = () => {
     const { loginWithRedirect } = useAuth0();
+    const { user, isAuthenticated } = useAuth0();
+
 
     const navigate = useNavigate();
     
