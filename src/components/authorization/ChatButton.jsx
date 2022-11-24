2e5e3c015751f435b70cde437ef3f5c8692f3b87
Luciana Margoto <margotoluciana@gmail>
2022-11-24 15:28:15 +0100

    changed route for prod

--- src/components/authorization/ChatButton.jsx
+++ src/components/authorization/ChatButton.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const PlaceAdButton = () => {
+const BASE_URL = process.env.REACT_APP_BACKEND_BASE_SERVER_URL;
+
+
+const ChatButton = () => {
     const { loginWithRedirect } = useAuth0();
 
-    return <button onClick={() => loginWithRedirect({ redirectUri: 'http://localhost:3000/chat' })}>Chat</button>;
+    return <button onClick={() => loginWithRedirect({ redirectUri: `${BASE_URL}/chat` })}>Chat</button>;
 }
 
-export default PlaceAdButton
+export default ChatButton
--- src/components/authorization/PlaceAdButton.jsx
+++ src/components/authorization/PlaceAdButton.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import * as UsersApi from '../../api/UsersApi';
+import { useNavigate } from "react-router-dom";
+
+const BASE_URL = process.env.REACT_APP_BACKEND_BASE_SERVER_URL;
 
 const PlaceAdButton = () => {
     const { loginWithRedirect } = useAuth0();
 
-    return <button onClick={() => loginWithRedirect({ redirectUri: 'http://localhost:3000/searchform' })}>Sell Card</button>;
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+      const checkUser = async () => {
+        if (isAuthenticated) {
+          const userId = await UsersApi.createUserIfNotExist(user);
+          window.localStorage.setItem('userId', userId);
+          navigate('/');
+        }
+      }
+  
+      checkUser();
+    }, [isAuthenticated, user, navigate]);
+
+    return <button onClick={() => loginWithRedirect({ redirectUri: `${BASE_URL}/searchform` })}>Sell Card</button>;
 }
 
 export default PlaceAdButton
