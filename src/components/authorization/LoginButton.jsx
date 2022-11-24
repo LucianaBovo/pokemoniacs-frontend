95228378e58144471ca9ed604de7e42d87b9a6e4
Luciana Margoto <margotoluciana@gmail>
2022-11-24 15:11:51 +0100

    added env var to root route

--- src/components/authorization/LoginButton.jsx
+++ src/components/authorization/LoginButton.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react';
 
+const BASE_URL = process.env.REACT_APP_BACKEND_BASE_SERVER_URL;
+
+
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
-    return <button onClick={() => loginWithRedirect({ redirectUri: 'http://localhost:3000/afterlogin' })}>Log In</button>;
+    return <button onClick={() => loginWithRedirect({ redirectUri: `${BASE_URL}/afterlogin` })}>Log In</button>;
 }
 
 export default LoginButton
