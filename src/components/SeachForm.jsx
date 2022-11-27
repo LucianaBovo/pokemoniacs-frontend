4822e928c661d2f0870819d3f36624d04e298d19
salt-dev <salt-dev@saltdevsAir69.home>
2022-11-27 18:17:04 +0100

    category WIP

--- src/components/SeachForm.jsx
+++ src/components/SeachForm.jsx
@@ -3,15 +3,16 @@ import { useState } from "react";
 import PokeCard from "./PokeCard";
 import "./SearchForm.css";
 import Layout from "./layout/Layout";
+import PokeCategory from "./pokemon/PokeCategory";
 const SearchForm = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [cards, setCards] = useState([]);
-
+  const [category, setCategory] = useState([])
   const getCards = async () => {
     try {
       if (searchTerm !== "") {
         const response = await fetch(
-          `https://api.pokemontcg.io/v1/cards?name=${searchTerm}`
+          `https://api.pokemontcg.io/v1/cards?name=${searchTerm}&types=${category.join(',') || ''}`
         );
         const data = await response.json();
         console.log(data);
@@ -24,6 +25,15 @@ const SearchForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     getCards();
+  };
+  const handleCategoryChange = e => {
+    const { value, checked } = e.target;
+
+    if (checked) {
+      setCategory(current => [...current, value]);
+    } else {
+      setCategory(category.filter(type => type !== value));
+    }
   };
   return (
     <Layout>
@@ -43,6 +53,7 @@ const SearchForm = () => {
             return <PokeCard card={card} key={index} />;
           })}
         </div>
+        <PokeCategory onInputChange={handleCategoryChange}/>
       </div>
     </Layout>
   );
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,6 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 import AuthBox from "../authorization/AuthBox";
 import PlaceAd from "../authorization/PlaceAdButton";
+import PokeCategory from "../pokemon/PokeCategory";
 // import ChatButton from '../authorization/ChatButton';
 
 import "./Header.css";
