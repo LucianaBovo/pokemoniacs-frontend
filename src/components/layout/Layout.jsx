a78cb3cf100eb4e85a8704408880dc09d4d8f4eb
Luciana Margoto <margotoluciana@gmail>
2022-11-27 10:50:36 +0100

    added missing  auth0 in layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react"; 
 
 import "./Layout.css";
 
 const Layout = ({ children }) => {
+  const {isAuthenticated} = useAuth0();
   return (
     <div className="layout">
       <Header />
